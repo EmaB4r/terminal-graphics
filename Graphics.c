2b2987371591d0bcf315b4e178dcd0c3f3ac84eb
EmaB4r <emanuelebar323@gmail.com>
2024-11-13 12:45:56 +0100

    renamed screen to canvas

--- Graphics.c
+++ Graphics.c
@@ -43,23 +43,23 @@ unsigned char getkey(FILE* stream){
 }
 
 
-void _draw_pixel(screen_t *screen, v2i_t p,  char c ,color_t px_color, color_t bg_color, enum gtype type){
-    if(p.x < 0 || p.y < 0 || p.x > screen->width-1 || p.y > screen->height-1) return;
-    screen->pixel[p.y][p.x].c=c;
-    screen->pixel[p.y][p.x].px_color=px_color;
-    screen->pixel[p.y][p.x].bg_color=bg_color;
+void _draw_pixel(canvas_t *canvas, v2i_t p,  char c ,color_t px_color, color_t bg_color, enum gtype type){
+    if(p.x < 0 || p.y < 0 || p.x > canvas->width-1 || p.y > canvas->height-1) return;
+    canvas->pixel[p.y][p.x].c=c;
+    canvas->pixel[p.y][p.x].px_color=px_color;
+    canvas->pixel[p.y][p.x].bg_color=bg_color;
 }
 
-void _draw_line(screen_t *screen, v2i_t p1, v2i_t p2, char c ,color_t px_color, color_t bg_color, enum gtype type){
+void _draw_line(canvas_t *canvas, v2i_t p1, v2i_t p2, char c ,color_t px_color, color_t bg_color, enum gtype type){
     if(p1.x==p2.x && p1.y==p2.y){
-        _draw_pixel(screen, p1, c, px_color, bg_color, type);
+        _draw_pixel(canvas, p1, c, px_color, bg_color, type);
         return;
     }
     if(p1.x==p2.x){
         if(p1.y>p2.y)
             p1.y^=p2.y^=p1.y^=p2.y;
         for(int y=p1.y; y<=p2.y; y++){
-            _draw_pixel(screen, (v2i_t){p1.x, y}, c, px_color, bg_color, type);
+            _draw_pixel(canvas, (v2i_t){p1.x, y}, c, px_color, bg_color, type);
         }
         return;
     }
@@ -71,50 +71,50 @@ void _draw_line(screen_t *screen, v2i_t p1, v2i_t p2, char c ,color_t px_color, 
     double q = (double)(p2.y-m*p2.x);
     for(int x=p1.x, y; x<=p2.x; x++){
         y=round_to_int(m*x+q);
-        _draw_pixel(screen, (v2i_t){x,y}, c, px_color, bg_color, type);
+        _draw_pixel(canvas, (v2i_t){x,y}, c, px_color, bg_color, type);
     }
 }
 
-void _draw_triangle(screen_t *screen, v2i_t p1, v2i_t p2, v2i_t p3,char c ,color_t px_color, color_t bg_color, enum gtype type){
-    _draw_line(screen, p1, p2, c ,px_color, bg_color, type);
-    _draw_line(screen, p2, p3, c ,px_color, bg_color, type);
-    _draw_line(screen, p3, p1, c ,px_color, bg_color, type);
+void _draw_triangle(canvas_t *canvas, v2i_t p1, v2i_t p2, v2i_t p3,char c ,color_t px_color, color_t bg_color, enum gtype type){
+    _draw_line(canvas, p1, p2, c ,px_color, bg_color, type);
+    _draw_line(canvas, p2, p3, c ,px_color, bg_color, type);
+    _draw_line(canvas, p3, p1, c ,px_color, bg_color, type);
 }
 
-void _draw_circle(screen_t * screen, v2i_t center, int radius, char c ,color_t px_color, color_t bg_color, enum gtype type){
+void _draw_circle(canvas_t * canvas, v2i_t center, int radius, char c ,color_t px_color, color_t bg_color, enum gtype type){
     v2i_t corner1={center.x-radius, center.y-radius};
     v2i_t corner2={center.x+radius, center.y+radius};
     for(int y = corner1.y; y<=corner2.y; ++y){
         for(int x = corner1.x; x<=corner2.x; ++x){
             if(square(x-center.x) + square(y-center.y) <= square(radius)){
-                _draw_pixel(screen, (v2i_t){x, y}, c, px_color, bg_color, type);
+                _draw_pixel(canvas, (v2i_t){x, y}, c, px_color, bg_color, type);
             }
         }
     }
 }
 
-void _draw_rectangle(screen_t *screen, v2i_t p1, v2i_t p2, char c, color_t px_color, color_t bg_color, enum gtype type, char fill){
+void _draw_rectangle(canvas_t *canvas, v2i_t p1, v2i_t p2, char c, color_t px_color, color_t bg_color, enum gtype type, char fill){
     if(fill){
         if(p1.x>p2.x) p1.x^=p2.x^=p1.x^=p2.x;
         if(p1.y>p2.y) p1.x^=p2.x^=p1.x^=p2.x;
         for(int i = p1.y; i<= p2.y; i++){
             for(int j = p1.x; j<=p2.x; j++){
-                screen->pixel[i][j].c=c;
-                screen->pixel[i][j].px_color=px_color;
-                screen->pixel[i][j].bg_color=bg_color;
+                canvas->pixel[i][j].c=c;
+                canvas->pixel[i][j].px_color=px_color;
+                canvas->pixel[i][j].bg_color=bg_color;
             }
         }
     }
     else{
-        _draw_line(screen, p1, (v2i_t){p2.x, p1.y}, c, px_color, bg_color, type);
-        _draw_line(screen, (v2i_t){p2.x, p1.y}, p2, c, px_color, bg_color, type);
-        _draw_line(screen, p2, (v2i_t){p1.x, p2.y}, c, px_color, bg_color, type);
-        _draw_line(screen, (v2i_t){p1.x, p2.y}, p1, c, px_color, bg_color, type);
+        _draw_line(canvas, p1, (v2i_t){p2.x, p1.y}, c, px_color, bg_color, type);
+        _draw_line(canvas, (v2i_t){p2.x, p1.y}, p2, c, px_color, bg_color, type);
+        _draw_line(canvas, p2, (v2i_t){p1.x, p2.y}, c, px_color, bg_color, type);
+        _draw_line(canvas, (v2i_t){p1.x, p2.y}, p1, c, px_color, bg_color, type);
     }
 }
 
 
-screen_t screen_init(int height, int width){
+canvas_t canvas_init(int height, int width){
     if(!terminal_initialized){
         terminal_initialized=1;
         save_terminal_state();
@@ -123,7 +123,7 @@ screen_t screen_init(int height, int width){
         timeout.tv_sec = 0;
         timeout.tv_usec = 1000;
     }
-    screen_t s;
+    canvas_t s;
     s.height=height;
     s.width=width;
     s.pixel=calloc(height, sizeof(pixel_t*));
@@ -134,7 +134,7 @@ screen_t screen_init(int height, int width){
     return s;
 }
 
-void screen_copy(screen_t * dest, screen_t * src){
+void canvas_copy(canvas_t * dest, canvas_t * src){
     if(dest->height==src->height && dest->width==src->width){
         for(int i = 0; i < src->height; ++i){
             for(int j = 0; j < src->width; ++j){
@@ -144,60 +144,60 @@ void screen_copy(screen_t * dest, screen_t * src){
     }
 }
 
-void screen_draw_gpixel(screen_t *screen, v2i_t p, color_t color){
-    _draw_pixel(screen, p, ' ', color, color, graphics);
+void canvas_draw_gpixel(canvas_t *canvas, v2i_t p, color_t color){
+    _draw_pixel(canvas, p, ' ', color, color, graphics);
 }
 
-void screen_draw_apixel(screen_t *screen, v2i_t p,  char c ,color_t px_color, color_t bg_color){
-    _draw_pixel(screen, p, c, px_color, bg_color, ascii);
+void canvas_draw_apixel(canvas_t *canvas, v2i_t p,  char c ,color_t px_color, color_t bg_color){
+    _draw_pixel(canvas, p, c, px_color, bg_color, ascii);
 }
 
-void screen_draw_gline(screen_t *screen, v2i_t p1, v2i_t p2, color_t bg_color){
-    _draw_line(screen, p1, p2, 'l', bg_color, bg_color, graphics);
+void canvas_draw_gline(canvas_t *canvas, v2i_t p1, v2i_t p2, color_t bg_color){
+    _draw_line(canvas, p1, p2, 'l', bg_color, bg_color, graphics);
 }
 
-void screen_draw_aline(screen_t *screen, v2i_t p1, v2i_t p2, char c, color_t px_color, color_t bg_color){
-    _draw_line(screen, p1, p2, c, px_color, bg_color, ascii);
+void canvas_draw_aline(canvas_t *canvas, v2i_t p1, v2i_t p2, char c, color_t px_color, color_t bg_color){
+    _draw_line(canvas, p1, p2, c, px_color, bg_color, ascii);
 }
 
-void screen_draw_gtriangle(screen_t * screen, v2i_t p1, v2i_t p2, v2i_t p3, color_t bg_color){
-    _draw_triangle(screen, p1, p2, p3, 't', bg_color, bg_color, graphics);
+void canvas_draw_gtriangle(canvas_t * canvas, v2i_t p1, v2i_t p2, v2i_t p3, color_t bg_color){
+    _draw_triangle(canvas, p1, p2, p3, 't', bg_color, bg_color, graphics);
 }
 
-void screen_draw_atriangle(screen_t * screen, v2i_t p1, v2i_t p2, v2i_t p3, color_t px_color,color_t bg_color){
-    _draw_triangle(screen, p1, p2, p3, 't', px_color, bg_color, ascii);
+void canvas_draw_atriangle(canvas_t * canvas, v2i_t p1, v2i_t p2, v2i_t p3, color_t px_color,color_t bg_color){
+    _draw_triangle(canvas, p1, p2, p3, 't', px_color, bg_color, ascii);
 }
 
-void screen_draw_gcircle(screen_t * screen, v2i_t center, int radius, color_t bg_color){
-    _draw_circle(screen,center, radius, 'c', bg_color, bg_color, graphics);
+void canvas_draw_gcircle(canvas_t * canvas, v2i_t center, int radius, color_t bg_color){
+    _draw_circle(canvas,center, radius, 'c', bg_color, bg_color, graphics);
 }
 
-void screen_draw_acircle(screen_t * screen, v2i_t center, int radius, char c ,color_t px_color, color_t bg_color){;
-    _draw_circle(screen,center, radius, c, px_color, bg_color, ascii);
+void canvas_draw_acircle(canvas_t * canvas, v2i_t center, int radius, char c ,color_t px_color, color_t bg_color){;
+    _draw_circle(canvas,center, radius, c, px_color, bg_color, ascii);
 }
 
-void screen_draw_arectangle(screen_t *screen, v2i_t p1, v2i_t p2, char c, color_t px_color, color_t bg_color, char fill){
-    _draw_rectangle(screen, p1, p2, c, px_color, bg_color, ascii, fill);
+void canvas_draw_arectangle(canvas_t *canvas, v2i_t p1, v2i_t p2, char c, color_t px_color, color_t bg_color, char fill){
+    _draw_rectangle(canvas, p1, p2, c, px_color, bg_color, ascii, fill);
 }
-void screen_draw_grectangle(screen_t *screen, v2i_t p1, v2i_t p2, color_t bg_color, char fill){
-    _draw_rectangle(screen, p1, p2, ' ' , bg_color, bg_color, graphics, fill);
+void canvas_draw_grectangle(canvas_t *canvas, v2i_t p1, v2i_t p2, color_t bg_color, char fill){
+    _draw_rectangle(canvas, p1, p2, ' ' , bg_color, bg_color, graphics, fill);
 }
 
-void _show(screen_t screen, enum gtype type){
-    for(int y=0; y<screen.height; y++){
-        for(int x=0; x<screen.width; x++){
+void _show(canvas_t canvas, enum gtype type){
+    for(int y=0; y<canvas.height; y++){
+        for(int x=0; x<canvas.width; x++){
             if(type == ascii){
-                if(screen.pixel[y][x].c != 0) {
+                if(canvas.pixel[y][x].c != 0) {
                     terminal_set_rgb_background(
-                    screen.pixel[y][x].bg_color.r,
-                    screen.pixel[y][x].bg_color.g,
-                    screen.pixel[y][x].bg_color.b);
+                    canvas.pixel[y][x].bg_color.r,
+                    canvas.pixel[y][x].bg_color.g,
+                    canvas.pixel[y][x].bg_color.b);
                     terminal_set_rgb_text(
-                    screen.pixel[y][x].px_color.r,
-                    screen.pixel[y][x].px_color.g,
-                    screen.pixel[y][x].px_color.b);
+                    canvas.pixel[y][x].px_color.r,
+                    canvas.pixel[y][x].px_color.g,
+                    canvas.pixel[y][x].px_color.b);
                     
-                    putc(screen.pixel[y][x].c, stdout);
+                    putc(canvas.pixel[y][x].c, stdout);
                 }
                 else {
                     putc(' ', stdout);
@@ -205,9 +205,9 @@ void _show(screen_t screen, enum gtype type){
             }
             else{
                 terminal_set_rgb_background(
-                screen.pixel[y][x].bg_color.r,
-                screen.pixel[y][x].bg_color.g,
-                screen.pixel[y][x].bg_color.b);
+                canvas.pixel[y][x].bg_color.r,
+                canvas.pixel[y][x].bg_color.g,
+                canvas.pixel[y][x].bg_color.b);
                 fputs("  ", stdout);
             }
             terminal_reset_color();
@@ -215,30 +215,30 @@ void _show(screen_t screen, enum gtype type){
         }
         putc('\n', stdout);
     }
-    terminal_cursor_left(screen.width);
-    terminal_cursor_up(screen.height);
+    terminal_cursor_left(canvas.width);
+    terminal_cursor_up(canvas.height);
 }
 
 
-void screen_gshow(screen_t screen){
-    _show(screen, graphics);
+void canvas_gshow(canvas_t canvas){
+    _show(canvas, graphics);
 }
 
-void screen_ashow(screen_t screen){
+void canvas_ashow(canvas_t canvas){
     terminal_set_cursor_pos(2, 1);
-    for(int y=0; y<screen.height; y++){
-        for(int x=0; x<screen.width; x++){
+    for(int y=0; y<canvas.height; y++){
+        for(int x=0; x<canvas.width; x++){
             
             terminal_set_rgb_background(
-            screen.pixel[y][x].bg_color.r,
-            screen.pixel[y][x].bg_color.g,
-            screen.pixel[y][x].bg_color.b);
+            canvas.pixel[y][x].bg_color.r,
+            canvas.pixel[y][x].bg_color.g,
+            canvas.pixel[y][x].bg_color.b);
             terminal_set_rgb_text(
-            screen.pixel[y][x].px_color.r,
-            screen.pixel[y][x].px_color.g,
-            screen.pixel[y][x].px_color.b);
+            canvas.pixel[y][x].px_color.r,
+            canvas.pixel[y][x].px_color.g,
+            canvas.pixel[y][x].px_color.b);
             
-            putc(screen.pixel[y][x].c, stdout);
+            putc(canvas.pixel[y][x].c, stdout);
             fflush(stdout);
         }
         putc('\n', stdout);
